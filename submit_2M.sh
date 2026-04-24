968b968b130e5069cbac695958b06fcfa1ddd0ab
Shivam Arora <37779038+arshiv-web@users.noreply.github.com>
2026-04-24 12:51:25 -0400

    Update algorithms and submission logic in submit_2M.sh

--- submit_2M.sh
+++ submit_2M.sh
@@ -4,7 +4,8 @@ ACCOUNTS=(
   ece567w26_class
 )
 
-ALGOS=("mappo" "ippo")
+ALGOS=("adaptive_mappo" "mappo" "ippo" "fixed_alpha_mappo")
+
 
 TASKS=(
   "vmas/navigation"
@@ -16,18 +17,36 @@ TASKS=(
 SEEDS=(0 1 2)
 
 i=0
-for task in "${TASKS[@]}"; do
-  for algo in "${ALGOS[@]}"; do
-    for seed in "${SEEDS[@]}"; do
+for algo in "${ALGOS[@]}"; do
+  for seed in "${SEEDS[@]}"; do
+    for task in "${TASKS[@]}"; do
       account=${ACCOUNTS[$((i % ${#ACCOUNTS[@]}))]}
       task_tag=$(echo "$task" | tr '/' '_')
+      if [[ "$ALGO" == "mappo" || "$ALGO" == "ippo" || "$ALGO" == "adaptive_mappo" ]]; then
+        sbatch \
+          --account="$account" \
+          --job-name=rl1m_${task_tag}_${algo}_s${seed} \
+          --export=ALL,ACCOUNT="$account",ALGO="$algo",TASK="$task",TASK_TAG="$task_tag",SEED="$seed" \
+          run_2m_single.sh
+      else
+        sbatch \
+          --account="$account" \
+          --job-name=rl1m_${task_tag}_${algo}_s${seed} \
+          --export=ALL,ACCOUNT="$account",ALGO="$algo",TASK="$task",TASK_TAG="$task_tag",SEED="$seed",ALPHA="0.25" \
+          run_2m_single.sh
+        
+        sbatch \
+          --account="$account" \
+          --job-name=rl1m_${task_tag}_${algo}_s${seed} \
+          --export=ALL,ACCOUNT="$account",ALGO="$algo",TASK="$task",TASK_TAG="$task_tag",SEED="$seed",ALPHA="0.50" \
+          run_2m_single.sh
 
-      sbatch \
-        --account="$account" \
-        --job-name=rl1m_${task_tag}_${algo}_s${seed} \
-        --export=ALL,ACCOUNT="$account",ALGO="$algo",TASK="$task",TASK_TAG="$task_tag",SEED="$seed" \
-        run_2m_single.sh
-
+        sbatch \
+          --account="$account" \
+          --job-name=rl1m_${task_tag}_${algo}_s${seed} \
+          --export=ALL,ACCOUNT="$account",ALGO="$algo",TASK="$task",TASK_TAG="$task_tag",SEED="$seed",ALPHA="0.75" \
+          run_2m_single.sh
+      fi
       echo "Submitted 2M OFFLINE: $task $algo seed=$seed on $account"
       i=$((i + 1))
     done
